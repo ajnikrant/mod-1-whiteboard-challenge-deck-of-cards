3bfe2e405f26114afb48947823d9e2aca26af330
Amanda <ajnikrant@gmail.com>
2021-03-08 09:46:42 -0500

    commit tree 7bdecf77be0cc510f50619a9461ea6ccc8ecf8df

--- solution.rb
+++ solution.rb
@@ -1,7 +1,25 @@
+require 'pry'
+
 class Deck
+
+    attr_accessor :cards
+
+    def initialize
+      @cards= []
+
+        # @cards << Card.new(suit, rank)
+    end
 
 end
 
 class Card
+    attr_reader :suit, :rank
+    # binding.pry
+    def initialize(suit, rank)
+      @suit= suit
+      @rank = rank
 
+    end
 end
+
+card1=Card.new(9, "clubs")
